b30ddf0aac1048a72300266a9d48e7a32c2bdc1e
cakir <cakiroglufeyyaz@hotmail.com>
2024-03-29 21:03:02 +0100

    Springboot Demo Day04

---
+++ spring-07-SpringBoot-Demo/src/main/java/com/cydeo/model/Comment.java
@@ -0,0 +1,9 @@
+package spring;
+
+import lombok.Data;
+
+@Data
+public class Comment {
+    private String author;
+    private String text;
+}
---
+++ spring-07-SpringBoot-Demo/src/main/java/com/cydeo/repository/CommentRepository.java
@@ -0,0 +1,7 @@
+package spring;
+
+import com.cydeo.model.Comment;
+
+public interface CommentRepository {
+    void storeComment(Comment comment);
+}
